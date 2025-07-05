3f656c83d15d3a21aea73701bf05d6bdc8601ee4
SachithShilshan <91172509+SachithShilshan@users.noreply.github.com>
2025-07-06 00:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,34 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Load models
+lr_model = joblib.load('coupon_model_lr.pkl')
+dt_model = joblib.load('coupon_model_dt.pkl')
+features = joblib.load('model_features.pkl')
 
 @app.route('/')
 def home():
-    return "🎯 Coupon Redemption Prediction API is running!"
+    return "Coupon Redemption Prediction API"
 
+@app.route('/predict', methods=['POST'])
+def predict_lr():
+    try:
+        input_data = pd.DataFrame([request.json])[features]
+        pred = lr_model.predict(input_data)[0]
+        proba = lr_model.predict_proba(input_data)[0][1]
+        return jsonify({'model': 'Logistic Regression', 'prediction': int(pred), 'probability': float(proba)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/predict_dt', methods=['POST'])
+def predict_dt():
+    try:
+        input_data = pd.DataFrame([request.json])[features]
+        pred = dt_model.predict(input_data)[0]
+        proba = dt_model.predict_proba(input_data)[0][1]
+        return jsonify({'model': 'Decision Tree', 'prediction': int(pred), 'probability': float(proba)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
